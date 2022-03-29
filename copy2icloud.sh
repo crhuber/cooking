42195c712d41b5adf6228416037ab62dd0e2b82b
Craig Huber <craig.a.huber@gmail.com>
2022-03-29 13:17:27 +0200

    rename to copy

---
+++ copy2icloud.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -vr recipes/* "/Users/$USER/Library/Mobile Documents/iCloud~org~cooklang~cooklangapp/Documents/"
--- move2icloud.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp -r recipes/* "/Users/$USER/Library/Mobile Documents/iCloud~org~cooklang~cooklangapp/Documents/"
