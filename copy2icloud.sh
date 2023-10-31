c3bd6a6e12472b59e30d97a6ba62eef47d68886d
crhuber <crhuber@users.noreply.github.com>
2023-10-31 19:05:22 +0100

    refactor some recipies

--- copy2icloud.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp -vr recipes/* "/Users/$USER/Library/Mobile Documents/iCloud~org~cooklang~cooklangapp/Documents/"
---
+++ sync2icloud.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -avh --delete recipes/* "/Users/$USER/Library/Mobile Documents/iCloud~org~cooklang~cooklangapp/Documents/"
